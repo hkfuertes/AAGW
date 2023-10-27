467f4f16894fc15097da664a532f87434b112dc0
hkfuertes <hkfuertes@gmail.com>
2023-10-28 01:25:01 +0200

    changes

--- LOS18/softap_start.sh
+++ LOS18/softap_start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # Permissions on this file: 755, chown 0.0
 
-export SSID=AAWG
-export PASS_KEY=12334567890
+SSID=AAWG
+PASS_KEY=1234567890
 
+cmd -w wifi set-wifi-enabled disabled
 # cmd -w wifi start-softap $SSID wpa2 $PASS_KEY 5|bridged
 cmd -w wifi start-softap $SSID wpa2 $PASS_KEY
-
-cmd -w wifi start-softap AAWG wpa2 12334567890
 
