d77959706034694de19207863380bb8b9834b7ad
hkfuertes <hkfuertes@gmail.com>
2023-11-03 12:50:51 +0100

    helper App

---
+++ Helper/app/src/main/java/net/mfuertes/aagw/helper/BootCompletedReceiver.kt
@@ -0,0 +1,13 @@
+package net.mfuertes.aagw.helper
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompletedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("BootCompletedReceiver.onReceive() is not implemented")
+    }
+}
